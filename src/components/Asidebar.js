723900b7f9f035b0df6aafbe0e6e5c20be8fcb3f
SalvasDev <salsanzmelc@gmail.com>
2022-09-24 10:33:39 -0500

    otra correct aside

--- src/components/Asidebar.js
+++ src/components/Asidebar.js
@@ -18,7 +18,7 @@ import styled from '@emotion/styled'
 
 const Container = styled.div`
     background-color: var(--bluedark);
-    height: 100vh;
+    height: 100%;
 
 
       @media (max-width: 510px) {
